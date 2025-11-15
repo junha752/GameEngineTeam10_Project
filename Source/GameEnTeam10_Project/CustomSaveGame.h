2f4f8f5e951c589a71ff9377bdf55285e406dbb4
oZone0531 <100321104+oZone0531@users.noreply.github.com>
2025-11-15 12:29:16 +0900

    Update CustomSaveGame.h

--- Source/GameEnTeam10_Project/CustomSaveGame.h
+++ Source/GameEnTeam10_Project/CustomSaveGame.h
@@ -14,7 +14,22 @@ class GAMEENTEAM10_PROJECT_API UCustomSaveGame : public USaveGame
 
 public:
 
-	int CurrentStage = 1;
+	// save current stage of game.
+	// 1: conversation, 2: progression, 3: combat
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Saved Player State")
+	int CurrentStage;
+
+	// save player character parameters from PlayerCharacter.h
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Saved Player State")
+	EPlayerWeapon CurrentWeapon;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Saved Player State")
+	float HealthPoints;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Saved Player State")
+	float Stamina;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Saved Player State")
+	int Strength;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Saved Player State")
+	int Armer;
 	
 	UFUNCTION(BlueprintCallable, Category = "SaveGame")int GetCurrentStage() const;
 	UFUNCTION(BlueprintCallable, Category = "SaveGame")void SetCurrentStage(int Stage);
